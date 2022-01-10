6046a813ad355d1579bd12f76de80221180b4d6b
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-01-10 19:03:52 +0000

    Added the Navigation component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Navigation } from "../Navigation/Navigation";
 import styling from "./Header.module.css";
 
 export const Header: React.FC = () => {
   return (
     <>
-      <div className={styling.container}></div>
+      <div className={styling.container}>
+        <Navigation />
+      </div>
     </>
   );
 };
