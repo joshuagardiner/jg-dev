3235158efc36c044b844df437029490a4e94c663
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-01-10 20:56:20 +0000

    Added the Skills component

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "@joshuagardiner/typescript-component-library";
 import { Summary } from "../Summary/Summary";
+import { Skills } from "../Skills/Skills";
 import styling from "./Body.module.css";
 
 export const Body: React.FC = () => {
@@ -10,6 +11,7 @@ export const Body: React.FC = () => {
         <div className={styling.content}>
           <Card marginTop={"-9.5vw"} width={"66%"}>
             <Summary />
+            <Skills />
           </Card>
         </div>
       </div>
---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text, Title } from "@joshuagardiner/typescript-component-library";
+import styling from "./Skills.module.css";
+
+export const Skills: React.FC = () => {
+  const skillsArray = [
+    "React",
+    "TypeScript",
+    "CI/CD",
+    "AWS",
+    "Azure",
+    "TDD",
+    "GitHub",
+    "Jest",
+    "Cypress",
+    "Terraform",
+    "Docker",
+  ];
+
+  const softSkillsArray = [
+    "Communication",
+    "Iniative",
+    "Leadership",
+    "Problem solving",
+    "Teamwork",
+  ];
+
+  return (
+    <>
+      <Title fontSize={"2vw"} marginTop={"5%"} text={"Skills"} />
+      <ul className={styling.skills}>
+        {skillsArray.map((skill) => (
+          <li key={skill}>{<Text fontSize={"1.125vw"} text={skill} />}</li>
+        ))}
+        {softSkillsArray.map((softSkill) => (
+          <li key={softSkill}>
+            {<Text fontSize={"1.125vw"} text={softSkill} />}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
