ba440b23fd01a8df1055a70dd003c939aed424e0
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-01-11 12:37:48 +0000

    Updated content within the Education component

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Card,
+  Text,
   Title,
   Title as Course,
   Title as Tenure,
@@ -14,7 +15,7 @@ export const Education: React.FC = () => {
       <div className={styling.container}>
         <Title fontSize={"2vw"} marginTop={"7.5%"} text={"Education"} />
         <Card width={"100%"}>
-          <Course fontSize={"1.25vw"} text={"BSc COMPUTING"} />
+          <Course fontSize={"1.25vw"} text={"BSc (Hons) COMPUTING"} />
           <University
             fontSize={"1vw"}
             text={"Manchester Metropolitan University"}
@@ -25,6 +26,27 @@ export const Education: React.FC = () => {
             text={"September '11 - June '16"}
             textTransform={"uppercase"}
           />
+          <Card marginTop={"1vw"} width={"100%"}>
+            <University
+              fontSize={"1vw"}
+              text={"The Brooksbank School and Sixth Form"}
+              textTransform={"uppercase"}
+            />
+            <Tenure
+              fontSize={".75vw"}
+              text={"September '03 - June '10"}
+              textTransform={"uppercase"}
+            />
+            <Text
+              fontSize={".75vw"}
+              text={"A-level's: Information technology, Geography"}
+            />
+            <Text
+              fontSize={".75vw"}
+              text={"AS-level's: Business studies, Physical education"}
+            />
+            <Text fontSize={".75vw"} text={"GCSE's: 10 [A-C]"} />
+          </Card>
         </Card>
       </div>
     </>
