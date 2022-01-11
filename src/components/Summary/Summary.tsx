a9f8a6365102d5a9fb5cb5ff46322f5ce300f55b
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-01-11 12:00:28 +0000

    Added content to the Summary component

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -41,7 +41,13 @@ export const Summary: React.FC = () => {
         fontSize={"1.125vw"}
         lineHeight={"2.5vw"}
         maxWidth={"75%"}
-        text={`You can write here details about one of your team members. You can give more details about what they do. Feel free to add some links for people to be able to follow them outside the site.`}
+        text={`An experienced software development engineer with a passion for developing web applications. Well-versed in a variety of technologies and writing code to create applications that are reliable and user-friendly.`}
+      />
+      <Text
+        fontSize={"1.125vw"}
+        lineHeight={"2.5vw"}
+        maxWidth={"75%"}
+        text={`A budding leader with a proven ability to mentor and motivate my peers, who can communicate confidently, problem-solve effectively and utilise initiative to deliver applications that are tailored to organizational needs using best practices and the best tool for the job.`}
       />
     </>
   );
