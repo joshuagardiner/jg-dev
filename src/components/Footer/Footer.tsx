12a5312b9206be643d50eb0b25455b474f5e537b
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-01-29 14:22:55 +0000

    Added comments to the Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,12 @@ import { IconButton } from "@joshuagardiner/typescript-component-library";
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 import styling from "./Footer.module.css";
 
+/**
+ * The Footer component is responsible for rendering the components
+ * that sit within the application Footer.
+ *
+ * @returns the Footer component
+ */
 export const Footer: React.FC = () => {
   return (
     <>
