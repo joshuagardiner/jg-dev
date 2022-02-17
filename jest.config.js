b63c2e7ec7b903a49389fc39432b7adc7f5a930a
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-02-17 16:44:28 +0000

    Bumped coverageThreshold's to 100

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: "./"
 });
 
 // Add any custom config to be passed to Jest
@@ -15,14 +15,14 @@ const customJestConfig = {
   coverageDirectory: "build/reports/coverage",
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
   },
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom"
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
