0702250ce8d2f26efbeb51af065ff9494b328e20
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-02-20 12:06:17 +0000

    Improved Data types returned from the Handler

---
+++ src/client/responses/ISummary.ts
@@ -0,0 +1,34 @@
+/**
+ * The response object the API returns.
+ */
+export interface ISummary {
+  /**
+   * The address.
+   */
+  address: string;
+
+  /**
+   * The email.
+   */
+  email: string;
+
+  /**
+   * The name.
+   */
+  name: string;
+
+  /**
+   * The role.
+   */
+  role: string;
+
+  /**
+   * The telephone.
+   */
+  telephone: string;
+
+  /**
+   * The text.
+   */
+  text: string[];
+}
