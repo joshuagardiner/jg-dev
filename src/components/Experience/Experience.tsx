2031daf685a1288b259e2ddc5f778777fd404e9c
joshuagardiner <Joshua.Gardiner@emishealth.com>
2022-02-23 14:39:49 +0000

    Restructure Experience Cards when viewing on mobile/tablet

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "@joshuagardiner/typescript-component-library";
+import { Card, Collection, CollectionColumn } from "@joshuagardiner/typescript-component-library";
 import { IExperienceProps } from "./IExperienceProps";
 import styling from "./Experience.module.css";
 
@@ -17,36 +17,38 @@ export const Experience: React.FC<IExperienceProps> = (props: IExperienceProps) 
       <h2 className={styling.title} data-testid={"experience-title"}>
         Experience
       </h2>
-      <Card width={"100%"}>
-        <h3 className={styling.role} data-testid={"experience-role"}>
-          {experiences[0].role}
-        </h3>
-        <h4 className={styling.company} data-testid={"experience-company"}>
-          {experiences[0].company}
-        </h4>
-        <h5 className={styling.tenure} data-testid={"experience-tenure"}>
-          {experiences[0].tenure}
-        </h5>
-        <p className={styling.content} data-testid={"experience-content"}>
-          {experiences[0].content[0]}
-          <br></br>
-          <br></br>
-          {experiences[0].content[1]}
-          <br></br>
-          <br></br>
-          {experiences[0].content[2]}
-          <br></br>
-          <br></br>
-          {experiences[0].content[3]}
-          <br></br>
-          <br></br>
-          {experiences[0].content[4]}
-          <br></br>
-          <br></br>
-          {experiences[0].content[5]}
-        </p>
-        <Card marginTop={"2vw"} width="100%">
-          <Card width={"50%"}>
+      <Collection>
+        <Card>
+          <h3 className={styling.role} data-testid={"experience-role"}>
+            {experiences[0].role}
+          </h3>
+          <h4 className={styling.company} data-testid={"experience-company"}>
+            {experiences[0].company}
+          </h4>
+          <h5 className={styling.tenure} data-testid={"experience-tenure"}>
+            {experiences[0].tenure}
+          </h5>
+          <p className={styling.content} data-testid={"experience-content"}>
+            {experiences[0].content[0]}
+            <br></br>
+            <br></br>
+            {experiences[0].content[1]}
+            <br></br>
+            <br></br>
+            {experiences[0].content[2]}
+            <br></br>
+            <br></br>
+            {experiences[0].content[3]}
+            <br></br>
+            <br></br>
+            {experiences[0].content[4]}
+            <br></br>
+            <br></br>
+            {experiences[0].content[5]}
+          </p>
+        </Card>
+        <CollectionColumn className={styling.cardColumnCollection}>
+          <Card className={styling.subExperience}>
             <h3 className={styling.role} data-testid={"experience-role"}>
               {experiences[1].role}
             </h3>
@@ -56,11 +58,11 @@ export const Experience: React.FC<IExperienceProps> = (props: IExperienceProps) 
             <h4 className={styling.tenure} data-testid={"experience-tenure"}>
               {experiences[1].tenure}
             </h4>
-            <p className={styling.subContent} data-testid={"experience-sub-content"}>
+            <p className={styling.content} data-testid={"experience-sub-content"}>
               {experiences[1].content[0]}
             </p>
           </Card>
-          <Card width={"50%"}>
+          <Card className={styling.subExperience}>
             <h3 className={styling.role} data-testid={"experience-role"}>
               {experiences[2].role}
             </h3>
@@ -70,12 +72,12 @@ export const Experience: React.FC<IExperienceProps> = (props: IExperienceProps) 
             <h4 className={styling.tenure} data-testid={"experience-tenure"}>
               {experiences[2].tenure}
             </h4>
-            <p className={styling.subContent} data-testid={"experience-sub-content"}>
+            <p className={styling.content} data-testid={"experience-sub-content"}>
               {experiences[2].content[0]}
             </p>
           </Card>
-        </Card>
-      </Card>
+        </CollectionColumn>
+      </Collection>
     </div>
   ) : (
     <></>
